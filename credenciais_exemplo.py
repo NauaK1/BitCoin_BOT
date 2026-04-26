dc86502b42cfd56898fb254a57efa277f0b2e98c
Kauan <gcolab017@gmail.com>
2026-04-26 14:48:14 -0300

    arquivo credenciais_exemplo foi adicionado

---
+++ credenciais_exemplo.py
@@ -0,0 +1,9 @@
+# Usage das credenciais
+
+USERNAME = "Coloque seu usuario"
+KEY = "Coloque sua chave"
+SECRET = "Coloque sua senha"
+
+# Faça isso no seu arquivo credenciais.py e dê um import no arquivo do projeto
+# ex: import credenciais.py
+# ex: username = credenciais.USERNAME
